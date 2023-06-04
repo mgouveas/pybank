b71936d4af4e4c475dcc210f4ea9841ea264876d
MatheusGouveaS <mateusgouveasilva@gmail.com>
2023-06-04 16:07:06 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sistema bancário.
+
+### Projeto criado no módulo de fundamentos do curso de Python da DIO.me. O Desafio consiste 
+#
+
+## Funcionalidades:
+  * Sacar
+  * Depositar
+  * Visualizar Extrato
+    
+## Regras de Negócio:
+  * Não é permitido depositar valores negativos
+  * registrar todos os saques e depositos realizados
+  * Limite de 3 saques por dia
+  * Limite de R$500,00 por saque
+  * Ao solicitar o extrato, deverá ser exibido todas as operações e o saldo atual da conta
+
---
+++ main.py
@@ -0,0 +1,103 @@
+menu = """
+====================PyBank====================
+
+===========Seja Bem-Vindo ao Pybank===========
+
+   Escolha uma das Opções do nosso sistema
+
+   1 - SAQUE            3 - EXTRATO
+   2 - DEPOSITO         0 - SAIR
+
+   Informações Importantes:
+    * Você pode realizar até 3 saques/dia
+    * Seu limite por saque é de R$500,00
+
+==============================================
+""";
+
+msg_saque = """
+Saque autorizado!
+
+Aguarde a contagem das notas....
+
+Operação concluída. Retire seus notas da máquina. 
+""";
+
+msg_deposito = """
+Insira as notas no local indicado.
+
+Aguarde a contagem das notas....
+
+Operação concluída. Depósito realizado com sucesso. 
+""";
+
+msg_extrato = """
+Registro das últimas operações realizadas nessa conta.
+
+Aguarde a exibição das informações em tela....
+
+Operação concluída. Confira abaixo o seu extrato. 
+""";
+
+print(menu);
+
+limite_saque = 500;
+numero_saque = 0;
+qnt_saque_dia = 3;
+
+saldo = 0;
+
+extrato_saque = []
+
+extrato_deposito = []
+
+opcao = int(input("Escolha a operação que deseja realizar: "));
+
+while (opcao != 0):
+
+    match opcao:
+        case 1:
+            if numero_saque < qnt_saque_dia:
+                print('SAQUE.\n');
+                saque = int(input('Valor do saque: '));
+                if saque <= saldo and saque <= limite_saque:
+                    numero_saque +=1
+                    saldo -= saque
+                    print(msg_saque);
+                    print(f'\n Saldo da conta: {saldo}');
+                    extrato_saque.append(f'R${saque},00');
+                    print(extrato_saque)
+                else:
+                    print("Operação negada. Você não tem saldo suficiente em sua conta.");
+            else:
+                print("Operação Negada. Limite de saque diário atingido.")
+
+        case 2:
+            print('DEPÓSITO \n');
+            deposito = int(input("Digite o valor a ser depositado: "));
+            if deposito > 0:
+                print(msg_deposito);
+                saldo += deposito
+                extrato_deposito.append(f'R${deposito},00')
+                print(extrato_deposito)
+            else:
+                print("Valor inválido. O valor mínimo de depósito é de R$1")
+
+
+        case 3:
+            print("EXTRATO \n");
+            print(msg_extrato);
+            print(f'Saldo atual: R${saldo}.\n\n');
+            print(f'Depositos: {extrato_deposito}\n');
+            print(f'Saques: {extrato_saque}\n');
+        
+        case _:
+            print("Opção inválida. \n\n")
+    
+    _ = str(input("Obrigado por usar nosso banco. Deseja fazer outra operação: [S] SIM | [N] Não > ")).upper()
+    
+    if _ == 'S':
+        opcao = int(input("\n\n Escolha a operação que deseja realizar: "));
+    else:
+        opcao = 0;
+        print("\n\nSistema sendo encerrado. Volte sempre!\n")
